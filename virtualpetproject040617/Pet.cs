255cf9ac82fa3a5e96d013a935dbdfdc7302db63
kalynndenton <dentonkalynn12@gmail.com>
2017-04-07 02:18:31 -0400

    first commit of virtual pet. Barebones

---
+++ virtualpetproject040617/Pet.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtualpetproject040617
+{
+    public class Pet
+    { //To start, I'll create a public class for the object "pet"
+
+        //These are the variables in the object "pet". 
+
+        private int hunger = 80;
+        private int energy = 100;
+        private int dirty = 0;
+        private int life = 200;
+        private int bored = 0;
+        public string petName = String.Empty; //I created an empty string for the pet name. It will take input from the user.
+
+        //I'd like to figure out a way to show the pet's life cycle. When pet life = value it will be a certain life stage.
+        //This can probably be done in main
+        
+        public Pet()
+        {
+
+        }
+
+        public Pet(string name, int hungerCount, int tiredCount, int dirtyCount, int lifeCount, int boredCount)
+        {
+            this.petName = name;
+            this.energy = tiredCount;
+            this.dirty = dirtyCount;
+            this.life = lifeCount;
+            this.bored = boredCount;
+        }
+
+        public void Feed()
+        {
+            Console.WriteLine("You feed " + petName + " They look happy!! ");
+            hunger += 15;
+            energy += 10;
+            life -= 10;
+            dirty += 10;
+            bored += 10;
+
+        }
+
+        public void Clean()
+        {
+            hunger += 0;
+            energy += 0;
+            life -= 10;
+            dirty -= 20;
+            bored += 10;
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("You play ball with " + petName );
+
+
+            hunger -= 20;
+            energy -= 10;
+            life -= 10;
+            dirty += 10;
+            bored -= 20;
+        }
+        public void ToBed()
+        {
+            Console.WriteLine("You put " + petName + " to sleep! " );
+
+
+            hunger += 15;
+            energy += 50;
+            life -= 10;
+            dirty += 10;
+            bored += 10;
+
+        }
+        public void Punish()
+        {
+            hunger += 0;
+            energy -= 20;
+            life -= 50;
+            dirty += 50;
+            bored += 10;
+
+            Console.WriteLine("Why would you do that?!");
+            Console.WriteLine("Don't you know that stressing them out will make them age fast!!");
+        }
+
+        public void petStage()
+        {
+            Console.WriteLine(life);
+        }
+
+        public int petHunger
+        {
+            get { return this.hunger; }
+            set { hunger = value; }
+        }
+
+        public int petEnergy
+        {
+            get { return this.energy; }
+            set { energy = value; }
+        }
+        public int petDirty
+        {
+            get { return this.dirty; }
+            set { dirty = value; }
+        }
+        public int petAge
+        {
+            get { return this.life; }
+            set { life = value; }
+        }
+        public int petBoredom
+        {
+            get { return this.bored; }
+            set { bored = value; }
+        }
+        public int PetStage
+        {
+            get { return this.life; }
+            set { life = value;}
+        }
+    }
+    }
+
+
---
+++ virtualpetproject040617/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtualpetproject040617
+{
+    class Program
+    {
+        static Pet myPet = new Pet();
+        static bool quit = false;
+        static bool isDead = false;
+
+
+        static void GetPetStatus()
+        {
+            Console.WriteLine("name: " + myPet.petName);
+            Console.WriteLine("hunger: " + myPet.petHunger);
+            Console.WriteLine("boredom: " + myPet.petBoredom);
+            Console.WriteLine("dirty: " + myPet.petDirty);
+            Console.WriteLine("exhaustion: " + myPet.petEnergy);
+        }
+
+
+        static void Quit()
+        {
+            Console.WriteLine("Thank you for playing!");
+            Console.WriteLine("Please press enter to exit!");
+            Console.ReadKey();
+        }
+
+        static string[] options = { "1. get pet age.", "2. feed", "3. play with", "4. clean up", "5. put to bed", "6. Punish", "7. Quit" };
+
+
+        static void Main(string[] args)
+        {
+            string userInput = string.Empty;
+
+
+            Console.WriteLine("Virtual Pet");
+
+            Console.WriteLine("Please name your pet!");
+
+            myPet.petName = Console.ReadLine();
+
+
+            while(quit == false && isDead == false)
+            {
+
+
+                for (int i = 1; i < (i + 2); i++)
+                {
+
+                    GetPetStatus();
+
+                    Console.WriteLine("----------------------------------");
+
+                    foreach (string option in options)
+                    {
+                        Console.WriteLine(option);
+                    }
+
+                    Console.WriteLine("--------------------------------");
+
+                    Console.WriteLine("Please choose a option");
+
+                    userInput = Console.ReadLine();
+                    Console.Clear();
+
+                    switch (userInput)
+                    {
+
+                        case "1":
+                            Console.WriteLine("Life: " + myPet.PetStage);
+                            break;
+                        case "2":
+                            if (myPet.petHunger > 80)
+                            {
+                                Console.WriteLine("They don't look hungry...");
+                            }
+                            else
+                            {
+                                myPet.Feed();
+                            }
+
+                            break;
+                        case "3":
+                            if (myPet.petBoredom <= 20)
+                            {
+                                Console.WriteLine("You throw a ball to " + myPet.petName + " it rolls right by them.\nThey look confused.");
+                            }
+                            else
+                            {
+                                myPet.Play();
+                            }
+                            break;
+                        case "4":
+                            if (myPet.petDirty <= 0)
+                            {
+                                Console.WriteLine("You can't clean what isn't there!!");
+                            }
+                            else
+                            {
+                                myPet.Clean();
+                            }
+                            break;
+                        case "5":
+                            if (myPet.petEnergy >= 80)
+                            {
+                                Console.WriteLine("They really aren't that tired!");
+                            }
+                            else
+                            {
+                                myPet.ToBed();
+                            }
+
+                            break;
+                        case "6":
+                            myPet.Punish();
+                            break;
+                        case "7":
+                            Program.Quit();
+                            quit = true;
+                            break;
+                        default:
+                            Console.WriteLine("Please choose a valid response!");
+                            break;
+
+                    }
+
+
+                    if (quit == true)
+                    {
+                        break;
+                    }
+
+                    if (myPet.PetStage == 0)
+
+                    {
+                        Console.WriteLine(myPet.petName + " has died");
+                        isDead = true;
+                    }
+
+                    if (isDead == true)
+                    {
+                        break;
+                    }
+
+                    }
+                
+
+                    }
+                }
+
+        }
+    }
+
