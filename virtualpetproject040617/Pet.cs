6dd4f7b06a84cb4ad1804414afa887b2b033c617
kalynndenton <dentonkalynn12@gmail.com>
2017-04-08 02:30:58 -0400

    new commit, still need to add a intro and a bit more depth

--- virtualpetproject040617/Pet.cs
+++ virtualpetproject040617/Pet.cs
@@ -11,7 +11,7 @@ namespace virtualpetproject040617
 
         //These are the variables in the object "pet". 
 
-        private int hunger = 80;
+        private int hunger = 100;
         private int energy = 100;
         private int dirty = 0;
         private int life = 200;
@@ -19,39 +19,40 @@ namespace virtualpetproject040617
         public string petName = String.Empty; //I created an empty string for the pet name. It will take input from the user.
 
         //I'd like to figure out a way to show the pet's life cycle. When pet life = value it will be a certain life stage.
-        //This can probably be done in main
         
-        public Pet()
+        
+        public Pet() //Default constructor
         {
 
         }
 
-        public Pet(string name, int hungerCount, int tiredCount, int dirtyCount, int lifeCount, int boredCount)
+        public Pet(string petName, int hunger, int tired, int dirty, int life, int bored)
         {
-            this.petName = name;
-            this.energy = tiredCount;
-            this.dirty = dirtyCount;
-            this.life = lifeCount;
-            this.bored = boredCount;
-        }
+            this.petName = petName;
+            this.energy = tired;
+            this.dirty = dirty;
+            this.life = life;
+            this.bored = bored;
+        } // I realized I didn't exactly need this constructor. But I decided to keep it anyways
 
-        public void Feed()
+        public void Feed() //Feeding method, this causes the stat changes for being fed.
         {
             Console.WriteLine("You feed " + petName + " They look happy!! ");
             hunger += 15;
             energy += 10;
-            life -= 10;
+            life -= 5;
             dirty += 10;
             bored += 10;
 
         }
 
-        public void Clean()
+        public void Clean() //Cleaning method
         {
+            Console.WriteLine("You clean up after " + petName);
             hunger += 0;
             energy += 0;
-            life -= 10;
-            dirty -= 20;
+            life -= 5;
+            dirty -= 10;
             bored += 10;
         }
 
@@ -61,7 +62,7 @@ namespace virtualpetproject040617
 
 
             hunger -= 20;
-            energy -= 10;
+            energy -= 40;
             life -= 10;
             dirty += 10;
             bored -= 20;
@@ -71,47 +72,44 @@ namespace virtualpetproject040617
             Console.WriteLine("You put " + petName + " to sleep! " );
 
 
-            hunger += 15;
-            energy += 50;
+            hunger -= 10;
+            energy += 60;
             life -= 10;
             dirty += 10;
             bored += 10;
 
         }
-        public void Punish()
+        public void Punish()//Please do not do this. 
         {
-            hunger += 0;
+            hunger -= 20;
             energy -= 20;
             life -= 50;
             dirty += 50;
-            bored += 10;
+            bored += 0;
 
             Console.WriteLine("Why would you do that?!");
             Console.WriteLine("Don't you know that stressing them out will make them age fast!!");
         }
 
-        public void petStage()
-        {
-            Console.WriteLine(life);
-        }
+       
 
-        public int petHunger
+        public int petHunger //Gets and sets the value for petHunger
         {
             get { return this.hunger; }
             set { hunger = value; }
         }
 
-        public int petEnergy
+        public int petEnergy //Gets and sets the value for petEnergy
         {
             get { return this.energy; }
             set { energy = value; }
         }
-        public int petDirty
+        public int petDirty//Gets and sets the value for petDirty
         {
             get { return this.dirty; }
             set { dirty = value; }
         }
-        public int petAge
+        public int petAge //gets and sets the petsAge... why did I do this twice?!?
         {
             get { return this.life; }
             set { life = value; }
@@ -120,11 +118,6 @@ namespace virtualpetproject040617
         {
             get { return this.bored; }
             set { bored = value; }
-        }
-        public int PetStage
-        {
-            get { return this.life; }
-            set { life = value;}
         }
     }
     }
--- virtualpetproject040617/Program.cs
+++ virtualpetproject040617/Program.cs
@@ -8,9 +8,9 @@ namespace virtualpetproject040617
 {
     class Program
     {
-        static Pet myPet = new Pet();
-        static bool quit = false;
-        static bool isDead = false;
+        static Pet myPet = new Pet();//Instantiated a new "pet" from the default constructor
+        static bool quit = false; // The user can quit at anytime in the program. quit is false so the program can continue looping.
+        static bool isDead = false;//Another way to end the program.
 
 
         static void GetPetStatus()
@@ -19,8 +19,8 @@ namespace virtualpetproject040617
             Console.WriteLine("hunger: " + myPet.petHunger);
             Console.WriteLine("boredom: " + myPet.petBoredom);
             Console.WriteLine("dirty: " + myPet.petDirty);
-            Console.WriteLine("exhaustion: " + myPet.petEnergy);
-        }
+            Console.WriteLine("energy levels: " + myPet.petEnergy);
+        } //A method made in Program class. Get's the pet's status to be used as a menu.
 
 
         static void Quit()
@@ -28,28 +28,28 @@ namespace virtualpetproject040617
             Console.WriteLine("Thank you for playing!");
             Console.WriteLine("Please press enter to exit!");
             Console.ReadKey();
-        }
+        }//If you choose to quit. This runs.
 
         static string[] options = { "1. get pet age.", "2. feed", "3. play with", "4. clean up", "5. put to bed", "6. Punish", "7. Quit" };
+        //A array to list the available options.
 
-
-        static void Main(string[] args)
+        static void Main(string[] args)//Program! START!!
         {
-            string userInput = string.Empty;
+            string userInput = string.Empty;//A variable for user input. Used for the switch statement below.
 
 
             Console.WriteLine("Virtual Pet");
 
             Console.WriteLine("Please name your pet!");
 
-            myPet.petName = Console.ReadLine();
+            myPet.petName = Console.ReadLine(); //throws petName a value to use.
 
 
-            while(quit == false && isDead == false)
+            while(quit == false && isDead == false)//a while loop that only works while these two bools are false (Is the pet dead or did the user say quit?)
             {
 
 
-                for (int i = 1; i < (i + 2); i++)
+                for (int i = 1; i < (i + 2); i++)//A for loop to keep the menu running until the pet dies or user quits.
                 {
 
                     GetPetStatus();
@@ -58,36 +58,50 @@ namespace virtualpetproject040617
 
                     foreach (string option in options)
                     {
-                        Console.WriteLine(option);
+                        Console.WriteLine(option); //list the options
                     }
 
                     Console.WriteLine("--------------------------------");
 
                     Console.WriteLine("Please choose a option");
 
-                    userInput = Console.ReadLine();
-                    Console.Clear();
+                    userInput = Console.ReadLine();//Throws a value into userInput.
+                    Console.Clear(); // Keeps things clean.
 
                     switch (userInput)
                     {
 
                         case "1":
-                            Console.WriteLine("Life: " + myPet.PetStage);
+                            Console.WriteLine("Life: " + myPet.petAge);
+                            if(myPet.petAge == 200)//The life stage feature I was so excited about. Only works at certain points in the pet's life cycle.
+                            {
+                                Console.WriteLine(myPet.petName + " is a baby!");
+                            } if (myPet.petAge == 100) //if life is 100, the pet is a baby.
+                            {
+                                Console.WriteLine(myPet.petName + " is a kid!");
+                            }if (myPet.petAge == 50) //if 50, an adult.
+                            {
+                                Console.WriteLine(myPet.petName + " is a grown up!");
+                            }if(myPet.petAge == 20) //pet may die soon.
+                            {
+                                Console.WriteLine(myPet.petName + " is in their sunset.");
+                            }
+
                             break;
                         case "2":
-                            if (myPet.petHunger > 80)
+                            if (myPet.petHunger > 80)//If the pet is "full", it won't eat.
                             {
                                 Console.WriteLine("They don't look hungry...");
-                            }
+                            } 
                             else
-                            {
+                            { //It will if it is.
                                 myPet.Feed();
                             }
 
                             break;
                         case "3":
                             if (myPet.petBoredom <= 20)
-                            {
+                            { //If it isn't bored. It won't play.
                                 Console.WriteLine("You throw a ball to " + myPet.petName + " it rolls right by them.\nThey look confused.");
                             }
                             else
@@ -97,7 +111,7 @@ namespace virtualpetproject040617
                             break;
                         case "4":
                             if (myPet.petDirty <= 0)
-                            {
+                            { //If it's not dirty. You can't clean.
                                 Console.WriteLine("You can't clean what isn't there!!");
                             }
                             else
@@ -107,7 +121,7 @@ namespace virtualpetproject040617
                             break;
                         case "5":
                             if (myPet.petEnergy >= 80)
-                            {
+                            {//They won't sleep if they aren't tired.
                                 Console.WriteLine("They really aren't that tired!");
                             }
                             else
@@ -117,33 +131,36 @@ namespace virtualpetproject040617
 
                             break;
                         case "6":
-                            myPet.Punish();
+                            myPet.Punish();//This is mean. Don't do this. It will kill your pet.
                             break;
                         case "7":
-                            Program.Quit();
+                            Quit();//Runs quit and changes quit to true. This breaks the loop and ends the program.
                             quit = true;
                             break;
                         default:
                             Console.WriteLine("Please choose a valid response!");
-                            break;
+                            break; //Please choose a valid response!! D:
 
                     }
 
 
                     if (quit == true)
                     {
-                        break;
+                        break; //once quit is true, end the program.
                     }
 
-                    if (myPet.PetStage == 0)
+                    if (myPet.petAge == 0 || myPet.petHunger <= 0) //if the pet's life goes to 0 OR the pet's hunger goes to 0. The pet dies.
+                        //which ends the program.
 
                     {
+                        Console.Clear();
                         Console.WriteLine(myPet.petName + " has died");
                         isDead = true;
                     }
 
                     if (isDead == true)
                     {
+                        //if isDead is true. The program ends.
                         break;
                     }
 
